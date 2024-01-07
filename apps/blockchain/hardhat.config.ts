e8f2b23e6a7167005967db0c41aae756a201959d
xXValhallaCoderXx <renate.gouveia@gmail.com>
2024-01-07 15:58:28 +0800

    remove hardhat

--- apps/blockchain/hardhat.config.ts
+++
@@ -1,8 +0,0 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox-viem";
-
-const config: HardhatUserConfig = {
-  solidity: "0.8.23",
-};
-
-export default config;
