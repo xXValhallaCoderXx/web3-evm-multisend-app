71fae932b20b5d0b49669214ef7668c7d9e08658
xXValhallaXx <renate.gouveia@gmail.com>
2024-02-04 15:13:06 +0800

    Refactoring

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    rewrites: async () => {
+        return [
+            {
+                source: '/pages/:path*',
+                destination: '/:path*',
+            },
+        ]
+    },
+}
 
 module.exports = nextConfig
