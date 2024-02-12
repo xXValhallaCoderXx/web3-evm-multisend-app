ed5c963ec4e9d41823dcf5093805cbf2fa16aa22
xXValhallaXx <renate.gouveia@gmail.com>
2024-02-13 01:23:44 +0800

    Fix payment layout

--- apps/web/app/_shared/components/organisms/RecentTransactions.tsx
+++ apps/web/app/_shared/components/organisms/RecentTransactions.tsx
@@ -3,18 +3,28 @@ import { Card, CardHeader, CardBody, Flex, Box, Text } from "@chakra-ui/react";
 const RecentTransactionsCard = () => {
   const isUserSignedIn = false;
   return (
-    <Card bgColor="#201B43" w="full">
+    <Card
+      bgColor="secondary.700"
+      borderColor="secondary.500"
+      borderWidth={2}
+      w="full"
+    >
       <CardBody>
-        <Text color="white" fontSize="xl" fontWeight={600}>
+        <Text
+          color="secondary.200"
+          letterSpacing={0.9}
+          fontSize="xl"
+          fontWeight={600}
+        >
           Recent Transactions
         </Text>
         <Flex justifyContent="center" mt={4} mb={2}>
           {!isUserSignedIn && (
             <Flex alignItems="center" flexDir="column">
-              <Text color="white" as="i">
+              <Text color="timberwolf.100" fontWeight={600}>
                 Sign in to view recent transactions
               </Text>
-              <Text fontSize="sm" color="gray">
+              <Text fontSize="sm" color="timberwolf.500" as="i">
                 Connect your wallet, to see your past transactions
               </Text>
             </Flex>
